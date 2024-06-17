3df55757cec65dc1710f0cef431d4d1553fe2d48
Fernando Mumbach <fermuch@ayvu.net>
2024-06-17 12:20:32 -0300

    statically forcing to evaluate all types

--- lib/PPNet/src/PPNet.cpp
+++ lib/PPNet/src/PPNet.cpp
@@ -1,12 +1,31 @@
 #include "PPNet.h"
 
+#include <variant>
+
 using namespace PPNetwork;
 using namespace PPNetwork::Message;
 
+template<typename... Ts> struct always_false : std::false_type {};
+
 size_t PPNet::WriteMessage(AnyMessage msg)
 {
-  std::visit([](auto&& arg) {
-    // std::cout << "Writing message: " << arg << std::endl;
-  }, msg);
-  return 0;
+  SyncMessage syncMsg;
+
+  auto visitor = [&](auto &&arg)
+  {
+    using T = std::decay_t<decltype(arg)>;
+    if constexpr (std::is_same_v<T, SingleCounterMessage>)
+    {
+      syncMsg.code = MessageType::SingleCounterMessage;
+      syncMsg.data = std::forward<decltype(arg)>(arg);
+    }
+    else
+    {
+      static_assert(always_false<T>::value, "Unhandled type in WriteMessage");
+    }
+
+    return 0;
+  };
+
+  return std::visit(visitor, msg);
 }

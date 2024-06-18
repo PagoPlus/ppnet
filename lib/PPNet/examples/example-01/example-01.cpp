ca7e94d0c6c740a904f1b7678bb9fde90d5d8c3f
Fernando Mumbach <fermuch@ayvu.net>
2024-06-18 10:10:48 -0300

    added real example

--- lib/PPNet/examples/example-01/example-01.cpp
+++ lib/PPNet/examples/example-01/example-01.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
 #include <PPNet.h>
 
+#include <WiFi.h>
+
+#define SOFTWARE_VERSION 0x00001
+#define HARDWARE_REVISION 0x0003
+
+PPNetwork::PPNet ppnet(&Serial1, PPNetwork::WriteTargetType::SUNTECH);
+
 void setup()
 {
   Serial.begin(115200);
+  // We'll use Serial1 as our Suntech device
+  Serial1.begin(9600);
+
+  // start wifi so we have true random numbers
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
+
+  // get our unique ID
+  char ssid[64];
+  snprintf(ssid, sizeof(ssid), "%llX", ESP.getEfuseMac());
+
+  // Since we just booted, we send a HELLO message to the server
+  ppnet.WriteMessage(PPNetwork::Message::HelloMessage {
+    // our unique board ID
+    .uniqueId = ssid,
+    // the name of the board
+    .boardIdentifier = "DemoBoard",
+    // our current system version
+    .version = SOFTWARE_VERSION,
+    .boardVersion = HARDWARE_REVISION,
+    .bootId = esp_random(),
+  });
 }
 
 void loop()
